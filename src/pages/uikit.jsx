76c5a77db97a4205c039f7c3c705ee89450732a5
Yurii Kluchevsky <info@webcademy.ru>
2025-07-25 18:43:55 +0300

    Fixed header

--- src/pages/uikit.jsx
+++ src/pages/uikit.jsx
@@ -3,7 +3,7 @@ import UiKitButton from "./../uikit-components/uikit-Button";
 const UiKit = () => {
 	return (
 		<div className="container">
-			<h1>UiKit</h1>
+			<h1 style={{ fontSize: "32px", fontWeight: 300, marginBottom: "20px" }}>UiKit</h1>
 
 			<div className="components">
 				<h2>Components</h2>

794f08763a59e7d20e953eff24eec1e5a5a8ad4c
Yurii Kluchevsky <info@webcademy.ru>
2025-07-26 16:09:05 +0300

    Сверстал компонент Бейдж

--- src/pages/uikit.jsx
+++ src/pages/uikit.jsx
@@ -1,13 +1,15 @@
 import UiKitButton from "./../uikit-components/uikit-Button";
+import UiKitBadge from "./../uikit-components/uikit-Badge";
 
 const UiKit = () => {
 	return (
 		<div className="container">
 			<h1 style={{ fontSize: "32px", fontWeight: 300, marginBottom: "20px" }}>UiKit</h1>
 
-			<div className="components">
+			<div className="components" style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
 				<h2>Components</h2>
 				<UiKitButton />
+				<UiKitBadge />
 			</div>
 		</div>
 	);

8149628c00316907959ca7b14806bd65dfabf335
Aleksandr Prokudin <145838822+Prokuuudin@users.noreply.github.com>
2025-07-30 20:26:49 +0300

    Revert "Details item"

--- src/pages/uikit.jsx
+++ src/pages/uikit.jsx
@@ -1,17 +1,13 @@
 import UiKitButton from "./../uikit-components/uikit-Button";
-import UiKitBadge from "./../uikit-components/uikit-Badge";
-import UiKitDetailsItem from "./../uikit-components/uikit-DetailsItem";
 
 const UiKit = () => {
 	return (
 		<div className="container">
 			<h1 style={{ fontSize: "32px", fontWeight: 300, marginBottom: "20px" }}>UiKit</h1>
 
-			<div className="components" style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+			<div className="components">
 				<h2>Components</h2>
 				<UiKitButton />
-				<UiKitBadge />
-				<UiKitDetailsItem />
 			</div>
 		</div>
 	);

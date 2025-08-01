30d78778a41b608f6a6830993c4ed3f400d06a57
Alex-nzn <xzczzc@inbox.ru>
2025-08-01 12:54:15 +0500

    Поправил описание компонента Newsletter
    Изменил высоту компонента Button

--- src/uikit-components/uikit-Newsletter.jsx
+++ src/uikit-components/uikit-Newsletter.jsx
@@ -4,7 +4,7 @@ import styles from "./uikit.module.css";
 const UiKitNewsletter = () => {
     return (
         <div className={styles.component}>
-            <h3 className={styles.component__title}>Link</h3>
+            <h3 className={styles.component__title}>Newsletter</h3>
 
             <div className={styles.component__preview}>
                 <Newsletter />
@@ -13,10 +13,10 @@ const UiKitNewsletter = () => {
             <div className={styles.component__code}>
                 <pre>
                     <code>
-                        {`import Link from "../components/Link/Link";`}
+                        {`import Newsletter from "../components/Newsletter/Newsletter";`}
                         <br />
                         <br />
-                        {`<Link link="/magazine" title="magazine" />`}
+                        {`<Newsletter />`}
                     </code>
                 </pre>
             </div>

8ceea6018b45e968aeee1db02dadfc70e421caa6
Yurii Kluchevsky <info@webcademy.ru>
2025-08-01 17:31:16 +0300

    Пофиксил заголовки для секций в Header

--- src/uikit-components/uikit-Header.jsx
+++ src/uikit-components/uikit-Header.jsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header/Header";
 import styles from "./uikit.module.css";
-import img from "../img/Art&Life.jpg";
 
 const UikitHeader = () => {
     return (
@@ -9,8 +8,7 @@ const UikitHeader = () => {
             <h3 className={styles.component__title}>Header</h3>
 
             <div className={styles.component__preview}>
-                <Header  title={img}/>
-         
+                <Header title="art&life"/>
             </div>
 
             <div className={styles.component__code}>
@@ -19,10 +17,7 @@ const UikitHeader = () => {
                         {`import Header from "../components/Header/Header";`}
                         <br />
                         <br />
-                        {`<Header  />`}
-                        <br />
-                        <br />
-                        {`<Header title={img} />`}
+                        {`<Header title="art&life | authors | magazine | podcast" />`}
                     </code>
                 </pre>
             </div>

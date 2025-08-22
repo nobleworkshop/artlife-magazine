2c41b48b32af632609624c695c65a612e9caca1b
Yurii Kluchevsky <info@webcademy.ru>
2025-08-22 14:10:26 +0300

    Компонент сверстан. Апдатив присутствует.

---
+++ src/components/PodcastListItem/PodcastListItem.jsx
@@ -0,0 +1,25 @@
+import styles from "./style.module.css";
+import LinkTo from "@components/LinkTo/LinkTo";
+import DetailsItem from "@components/DetailsItem/DetailsItem";
+
+const PodcastListItem = ({ number, image, title, date, duration, link }) => {
+	return (
+		<div className={styles["podcastListItem"]}>
+			<div className={styles["podcastListItem__number"]}>{number}</div>
+
+			<div className={styles["podcastListItem__image"]}>
+				<img src={image} alt={title} />
+			</div>
+
+			<div className={styles["podcastListItem__title"]}>{title}</div>
+
+			<div className={styles["podcastListItem__info"]}>
+				<DetailsItem title="Date" value={date} />
+				<DetailsItem title="Duration" value={duration} />
+				<LinkTo title="Listen" href={link} />
+			</div>
+		</div>
+	);
+};
+
+export default PodcastListItem;

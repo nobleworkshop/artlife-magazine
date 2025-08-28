6ebbdb8deb8275b96ab18fcf9f28594404668ebe
Amindlog <amind@bk.ru>
2025-08-28 17:42:39 +0400

    Исправил linkTo поправил адаптивность

--- src/components/LinkTo/LinkTo.jsx
+++ src/components/LinkTo/LinkTo.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import styles from './linkTo.module.css';
-import arrowRight from '../../img/link-arrow--right.svg';
-import arrowLeft from '../../img/link-arrow--left.svg';
+import arrowRight from '@img/link-arrow--right.svg';
+import arrowLeft from '@img/link-arrow--left.svg';
 
-const LinkTo = ({ title, link, direction = "right", className }) => {
+const LinkTo = ({ title, link, direction = "right"}) => {
 	const isLeft = direction === "left";
 	const arrow = isLeft ? arrowLeft : arrowRight;
 
 	return (
-		<Link to={link} className={`${styles.link} ${className || ''}`}>
+		<Link to={link} className={styles.link}>
 			{isLeft && <img src={arrow} alt="Link arrow" className={styles.icon} />}
 			<span className={styles.text}>{title}</span>
 			{!isLeft && <img src={arrow} alt="Link arrow" className={styles.icon} />}

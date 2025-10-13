ba43ef2e0b03bb3a827450f6dd9c9607cbd17a50
Amindlog <amind@bk.ru>
2025-10-13 17:57:23 +0400

    добавление в компоненты последних изменений

--- src/components/Authors/Authors.jsx
+++ src/components/Authors/Authors.jsx
@@ -1,60 +1,16 @@
+import { useApi } from '../../hooks/useApi';
 import AuthorCard from '../AuthorCard/AuthorCard';
 import LinkTo from '../LinkTo/LinkTo';
 import SectionTitle from '../SectionTitle/SectionTitle';
 
 import styles from './authors.module.css';
-import img1 from '../../img/authors/1.png';
-import img2 from '../../img/authors/2.png';
-import img3 from '../../img/authors/3.png';
-import img4 from '../../img/authors/4.png';
-import img5 from '../../img/authors/5.png';
-import img6 from '../../img/authors/6.png';
 
 const Authors = () => {
-	const authors = [
-		{
-			id: 1,
-			name: 'Jakob Grønberg',
-			job: 'Artist',
-			city: 'Berlin',
-			img: img1,
-		},
-		{
-			id: 2,
-			name: 'Louise Jensen',
-			job: 'Artist',
-			city: 'Stockholm',
-			img: img2,
-		},
-		{
-			id: 3,
-			name: 'Anne Henry',
-			job: 'Photograph',
-			city: 'New York',
-			img: img3,
-		},
-		{
-			id: 4,
-			name: 'Anna Nielsen',
-			job: 'Columnists',
-			city: 'Copenhagen',
-			img: img4,
-		},
-		{
-			id: 5,
-			name: 'Jane Cooper',
-			job: 'Artist',
-			city: 'Berlin',
-			img: img5,
-		},
-		{
-			id: 6,
-			name: 'Cristofer Vaccaro',
-			job: 'Artist',
-			city: 'Lisbon',
-			img: img6,
-		},
-	];
+	const { data: authors, loading, error } = useApi('authors');
+
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>Error: {error}</div>;
+	if (!authors) return <div>No authors found</div>;
 
 	return (
 		<section className={styles.authors}>
--- src/components/WidgetMostPopular/WidgetMostPopular.jsx
+++ src/components/WidgetMostPopular/WidgetMostPopular.jsx
@@ -1,20 +1,42 @@
+import { useApi } from '../../hooks/useApi';
 import MostPopular from '../MostPopular/MostPopular';
 import Widget from '../Widget/Widget';
 
 const WidgetMostPopular = () => {
+	const { data: articles, loading, error } = useApi('articles');
+
+	if (loading)
+		return (
+			<Widget title="Most Popular">
+				<div>Loading...</div>
+			</Widget>
+		);
+	if (error)
+		return (
+			<Widget title="Most Popular">
+				<div>Error: {error}</div>
+			</Widget>
+		);
+	if (!articles)
+		return (
+			<Widget title="Most Popular">
+				<div>No articles found</div>
+			</Widget>
+		);
+
+	// Get the most popular articles (first 3)
+	const mostPopularArticles = articles.slice(0, 3);
+
 	return (
 		<Widget title="Most Popular">
-			<MostPopular
-				num="01"
-				title="Street art festival"
-				autor="Cristofer Vaccaro"
-			/>
-			<MostPopular num="02" title="Hope dies last" autor="Anne Henry" />
-			<MostPopular
-				num="03"
-				title="Artists who want to rise above"
-				autor="Anna Nielsen"
-			/>
+			{mostPopularArticles.map((article, index) => (
+				<MostPopular
+					key={article.id}
+					num={`${String(index + 1).padStart(2, '0')}`}
+					title={article.title}
+					autor={article.authorName}
+				/>
+			))}
 		</Widget>
 	);
 };
